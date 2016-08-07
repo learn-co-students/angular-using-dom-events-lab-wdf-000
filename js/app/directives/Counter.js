dbe2ffad25985c874ea4f1908321d9991f475ccc
lastromanticx <giladbarkan@live.com>
2016-08-07 07:41:53 -0400

    passed test

--- js/app/directives/Counter.js
+++ js/app/directives/Counter.js
@@ -1,15 +1,27 @@
 function Counter() {
 	return {
+    transclude: true,
 		template: [
 			'<div>',
 				'<h3>Counter</h3>',
 				'<div>Click anywhere to increment the counter!</div>',
-				'<div>Current count: {{ count }}</div>',
+				'<div>Current count: {{ ctrl.count }}</div>',
 			'</div>'
 		].join(''),
-		controller: function ($scope) {
-			$scope.count = 0;
-		}
+		controller: function () {
+			this.count = 0;
+		},
+    controllerAs: 'ctrl',
+    link: function(scope,elem,attrs,ctrl){
+      elem.on('click',function(){
+        ctrl.count += 1; 
+        scope.$digest();
+      });
+
+      scope.$on('destroy',function(){
+        elem.off();
+      });
+    }
 	}
 }
 
